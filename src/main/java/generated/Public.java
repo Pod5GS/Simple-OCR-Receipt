352fb3cb8e73f6ef1b2c8e08d96fdad65d72a62b
Adam Fleming <amfleming@gmail.com>
2017-08-30 18:58:15 -0400

    Fix up bugs introduced during class

--- src/main/java/generated/Public.java
+++ src/main/java/generated/Public.java
@@ -4,16 +4,8 @@
 package generated;
 
 
-import generated.tables.Receipts;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import javax.annotation.Generated;
 
-import org.jooq.Sequence;
-import org.jooq.Table;
 import org.jooq.impl.SchemaImpl;
 
 
@@ -30,10 +22,10 @@ import org.jooq.impl.SchemaImpl;
 @SuppressWarnings({ "all", "unchecked", "rawtypes" })
 public class Public extends SchemaImpl {
 
-	private static final long serialVersionUID = -372256953;
+	private static final long serialVersionUID = -270095461;
 
 	/**
-	 * The reference instance of <code>public</code>
+	 * The reference instance of <code>PUBLIC</code>
 	 */
 	public static final Public PUBLIC = new Public();
 
@@ -41,30 +33,6 @@ public class Public extends SchemaImpl {
 	 * No further instances allowed
 	 */
 	private Public() {
-		super("public");
-	}
-
-	@Override
-	public final List<Sequence<?>> getSequences() {
-		List result = new ArrayList();
-		result.addAll(getSequences0());
-		return result;
-	}
-
-	private final List<Sequence<?>> getSequences0() {
-		return Arrays.<Sequence<?>>asList(
-			Sequences.SYSTEM_SEQUENCE_43792EFC_EB7B_420F_B3B7_B190C42A9DE4);
-	}
-
-	@Override
-	public final List<Table<?>> getTables() {
-		List result = new ArrayList();
-		result.addAll(getTables0());
-		return result;
-	}
-
-	private final List<Table<?>> getTables0() {
-		return Arrays.<Table<?>>asList(
-			Receipts.RECEIPTS);
+		super("PUBLIC");
 	}
 }
