90c1ea03a173bbc80c3ee878a64b090de4fd80b0
Adam Fleming <amfleming@gmail.com>
2017-08-30 12:13:16 -0400

    Add sample generated

---
+++ sample_schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE receipts (
+  id INT UNSIGNED AUTO_INCREMENT,
+  uploaded TIME DEFAULT CURRENT_TIME(),
+  merchant VARCHAR(255),
+  amount DECIMAL(12,2),
+  receipt_type INT UNSIGNED,
+
+  PRIMARY KEY (id)
+);
