20f8abb4bbca01048a3868aaea65eab89347614d
Andy Wei <wcincredible@live.com>
2017-09-03 17:57:14 -0400

    add table tag to sqlschema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,3 +7,11 @@ CREATE TABLE receipts (
 
   PRIMARY KEY (id)
 );
+
+CREATE TABLE tags (
+  id INT UNSIGNED AUTO_INCREMENT,
+  receipt_id INT UNSIGNED,
+  name VARCHAR(255),
+
+  PRIMARY KEY (id)
+);
